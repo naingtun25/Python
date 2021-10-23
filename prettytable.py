969dfa0b63b785fb7367e8e85a26ca647448f459
naingtun25 <89379816+naingtun25@users.noreply.github.com>
2021-10-24 02:29:51 +0800

    table

--- prettytable.py
+++ prettytable.py
@@ -1,8 +1,14 @@
-from prettytable import PrettyTable
-table = PrettyTable(['No', 'Name', 'ARC', 'Date', 'Phone'])
+#table = PrettyTable(['No', 'Name', 'ARC', 'Date', 'Phone'])
 
-table.align = '1'
+#table.align = '1'
 
-table.add_row(['1', 'Naing', '0909', 'Friday', '0909'])
+#table.add_row(['1', 'Naing', '0909', 'Friday', '0909'])
 
-print(table)
+#print(table)
+
+def funtable(row):
+   x = funtable.fields(['name', 'arc', 'date'])
+
+   funtable.add_row('naing', '01', '09')
+
+   print(x)
